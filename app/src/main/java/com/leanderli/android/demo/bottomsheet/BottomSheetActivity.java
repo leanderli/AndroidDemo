313a7fd03c8d2cf46ee360d3ba1cc71db483a7cb
leanderli <1838487641@qq.com>
2020-04-18 19:25:20 +0800

    更新BottomSheet的测试

--- app/src/main/java/com/leanderli/android/demo/bottomsheet/BottomSheetActivity.java
+++ app/src/main/java/com/leanderli/android/demo/bottomsheet/BottomSheetActivity.java
@@ -1,20 +1,69 @@
 package com.leanderli.android.demo.bottomsheet;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.leanderli.android.demo.R;
+import com.leanderli.android.demo.util.SystemUiController;
 
 public class BottomSheetActivity extends AppCompatActivity {
+    protected SystemUiController mSystemUiController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_sheet);
-        LinearLayout searchEdit = findViewById(R.id.search_edit);
-        LinearLayout searchContent = findViewById(R.id.search_content);
-        searchEdit.setAlpha(1);
-        searchContent.setAlpha(0);
+        updateUi(false);
+
+        View sheetTopBarView = findViewById(R.id.sheet_top_bar);
+
+        View sheetContentView = findViewById(R.id.sheet_content_view);
+
+        sheetContentView.setAlpha(0);
+        View mainContentView = findViewById(R.id.main_content_view);
+        mainContentView.setAlpha(1);
+
+        View bottomSheetView = findViewById(R.id.sheet_view);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(bottomSheetView);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        updateUi(false);
+//                        sheetTopBarView.setVisibility(View.VISIBLE);
+                        break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        updateUi(true);
+//                        sheetTopBarView.setVisibility(View.GONE);
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                mainContentView.setAlpha(1 - slideOffset * 2);
+//                sheetTopBarView.setAlpha(1 - slideOffset * 2);
+                sheetContentView.setAlpha(slideOffset);
+            }
+        });
+    }
+
+    void updateUi(boolean isLight) {
+        getSystemUiController().updateUiState(SystemUiController.UI_STATE_BASE_WINDOW, isLight);
+    }
+
+    public SystemUiController getSystemUiController() {
+        if (mSystemUiController == null) {
+            mSystemUiController = new SystemUiController(getWindow());
+        }
+        return mSystemUiController;
     }
 }
